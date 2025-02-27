76a232d99f519991bcf88fc08ae92e1e01f9b830
Borja Jiménez <truestylegs@gmail.com>
2025-02-27 22:45:39 +0100

    Update UserResource.php

--- app/Filament/Clusters/Go/Resources/UserResource.php
+++ app/Filament/Clusters/Go/Resources/UserResource.php
@@ -34,6 +34,7 @@ class UserResource extends Resource
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
+                    ->hidden(config('app.env') === 'demo')
                     ->required(),
                 Forms\Components\Textarea::make('attributes')
                     ->columnSpanFull(),
